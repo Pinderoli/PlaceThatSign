5ceb9c867b19a24776bc73265e6e47a514d46dcd
Oliver Pinder <oliverpinder@me.com>
2026-06-08 22:22:03 +0100

    Initial commit

---
+++ PlaceThatSign/PlaceThatSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceThatSignApp.swift
+//  PlaceThatSign
+//
+//  Created by Oliver Pinder on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlaceThatSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaceThatSignTests/PlaceThatSignTests.swift
@@ -0,0 +1,19 @@
+//
+//  PlaceThatSignTests.swift
+//  PlaceThatSignTests
+//
+//  Created by Oliver Pinder on 08/06/2026.
+//
+
+import Testing
+@testable import PlaceThatSign
+
+struct PlaceThatSignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
